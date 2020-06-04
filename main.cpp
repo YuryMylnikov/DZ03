58591a6bc6e2d9f5ad72ca7926ff877d5f277060
YuryMylnikov <otus.learn@gmail.com>
2020-06-04 23:22:09 +0300

    Review_01 DZ03
    This reverts commit 53cc94e17c46e62dec56b7b0392c9719e3a07fef.

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include "BlockAllocator.hpp"
+#include "SortedList.hpp"
+
+#include <iostream>
+#include <map>
+
+
+int factorial(int num)
+{
+	if (num <= 1)
+	{
+		return 1;
+	}
+
+	return num * factorial(num - 1);
+}
+
+
+int main()
+{
+	try
+	{
+		{
+			std::map<int, int> mFact{};
+
+			for (int i = 0; i < 10; ++i)
+			{
+				mFact[i] = factorial(i);
+			}
+
+			for (const auto& it : mFact)
+			{
+				std::cout << it.first << " " << it.second << std::endl;
+			}
+
+			std::cout << std::endl;
+		}
+
+		{
+			std::map<int, int, std::less<int>, BlockAllocator<int>> mFact{};
+
+			for (int i = 0; i < 10; ++i)
+			{
+				mFact[i] = factorial(i);
+			}
+
+			for (const auto& it : mFact)
+			{
+				std::cout << it.first << " " << it.second << std::endl;
+			}
+
+			std::cout << std::endl;
+		}
+
+		{
+			SortedList<int> slCont{};
+
+			for (int i = 0; i < 10; ++i)
+			{
+				slCont.Add(i);
+			}
+
+			for (const int& it : slCont)
+			{
+				std::cout << it << std::endl;
+			}
+
+			std::cout << std::endl;
+		}
+
+		{
+			SortedList<int, BlockAllocator<int>> slCont{};
+
+			for (int i = 0; i < 10; ++i)
+			{
+				slCont.Add(i);
+			}
+
+			for (const int& it : slCont)
+			{
+				std::cout << it << std::endl;
+			}
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	return 0;
+}
